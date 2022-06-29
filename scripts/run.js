ffe85341ff3454b8d9590c3e9cef232e5fb2336b
novickaya <novickaya@sputnikfund.ru>
2022-06-29 20:09:51 +0700

    create a contract that mints NFT

--- scripts/run.js
+++ scripts/run.js
@@ -3,6 +3,15 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy()
     await nftContract.deployed()
     console.log("Contract deployed to: ", nftContract.address)
+
+    // Call the function.
+    let txn = await nftContract.makeAnEpicNFT()
+    // Wait for it to be mined.
+    await txn.wait()
+
+    // mint another NFT
+    txn = await nftContract.makeAnEpicNFT()
+    await txn.wait()
 }
 
 const runMain = async () => {
